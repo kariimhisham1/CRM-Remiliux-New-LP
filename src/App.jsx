1cdce1dae3ac5f8a8b90197837d234a5863a3992
kariimhisham1 <kariimhisham1@gmail.com>
2026-06-28 22:10:50 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import ScrollTransition from './components/ScrollTransition';
 import WhyTeams from './components/WhyTeams';
+import AutomationEngine from './components/AutomationEngine';
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Navbar />
       <ScrollTransition />
       <WhyTeams />
+      <AutomationEngine />
     </div>
   );
 }
