ee5b41b2594b57220fffc280eba34445ec9ea518
kariimhisham1 <kariimhisham1@gmail.com>
2026-06-28 06:29:59 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import logo from '../assets/logo.png';
+import logo from '../assets/Logo.png';
 import './Navbar.css';
 
 const NAV_LINKS = ['Features', 'Solutions', 'Pricing', 'Resources', 'About'];
