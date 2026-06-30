4054c3daa61ba569ca1c90038d9e68d63a5e6246
kariimhisham1 <kariimhisham1@gmail.com>
2026-06-30 07:36:01 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import ScrollTransition from './components/ScrollTransition';
 import WhyTeams from './components/WhyTeams';
-
+import TeamPerformance from './components/TeamPerformance';
 export default function App() {
   return (
     <div className="app">
       <Navbar />
       <ScrollTransition />
       <WhyTeams />
+      <TeamPerformance />
     </div>
   );
 }
